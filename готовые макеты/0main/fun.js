0048d5fe3ee6c2801266fa388ce9823030d3cef9
AleksandrMart <159411827+Shur1k0@users.noreply.github.com>
2024-06-01 22:57:33 +0900

    Add files via upload

---
+++ готовые макеты/0main/fun.js
@@ -0,0 +1,17 @@
+function dropMenu() {
+    document.getElementById("dropDown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropButton')){
+
+        var dropdowns = document.getElementsByClassName("dropElements");
+        var i;
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
