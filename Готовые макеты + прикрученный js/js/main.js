9aab56e991abbc1b81d65a45bbf2d8cf9ef98852
AleksandrMart <159411827+Shur1k0@users.noreply.github.com>
2024-06-05 00:31:07 +0900

    Add files via upload
    Добавил js и немного прибрался в папках

---
+++ Готовые макеты + прикрученный js/js/main.js
@@ -0,0 +1,17 @@
+function dropMenu() {
+    document.getElementById("dropDown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.kont')){
+
+        var dropdowns = document.getElementsByClassName("dropKont");
+        var i;
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
